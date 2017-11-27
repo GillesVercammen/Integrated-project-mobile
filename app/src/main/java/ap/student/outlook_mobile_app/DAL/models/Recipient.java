176c8c2bd79ba93221f599277d9ddd9122cec3c7
GillesVercammen <gilles.vercammen@hotmail.be>
2017-11-27 15:23:43 +0100

    model for message
    added DAL models for message to try getting a list view of emails

---
+++ app/src/main/java/ap/student/outlook_mobile_app/DAL/models/Recipient.java
@@ -0,0 +1,18 @@
+package ap.student.outlook_mobile_app.DAL.models;
+
+/**
+ * Created by Gilles on 27-11-2017.
+ */
+
+public class Recipient {
+
+    private EmailAddress emailAddress;
+
+    public EmailAddress getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(EmailAddress emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
