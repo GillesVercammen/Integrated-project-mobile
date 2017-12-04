6329abd149351d7a5b0d1db31bdbb2c5ba6dbb5e
AlekVT <alek.vtichelen@gmail.com>
2017-12-04 14:50:28 +0100

    cleaned unnecessary elements from main activity layout

--- app/src/main/java/ap/student/outlook_mobile_app/MainActivity.java
+++ app/src/main/java/ap/student/outlook_mobile_app/MainActivity.java
@@ -1,22 +1,12 @@
 package ap.student.outlook_mobile_app;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.microsoft.identity.client.AuthenticationResult;
-import com.microsoft.identity.client.MsalClientException;
-import com.microsoft.identity.client.PublicClientApplication;
-import com.microsoft.identity.client.User;
 
 import org.json.JSONObject;
-
-import java.util.List;
 
 import ap.student.outlook_mobile_app.BLL.Authentication;
 import ap.student.outlook_mobile_app.BLL.GraphAPI;
@@ -25,8 +15,6 @@ import ap.student.outlook_mobile_app.Interfaces.AppCompatActivityRest;
 
 public class MainActivity extends AppCompatActivityRest {
     Button callGraphButton;
-    Button signOutButton;
-    Button mailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +26,6 @@ public class MainActivity extends AppCompatActivityRest {
         super.onCreate(savedInstanceState);
 
         callGraphButton = (Button) findViewById(R.id.callGraph);
-        signOutButton = (Button) findViewById(R.id.clearCache);
-        mailButton = (Button) findViewById(R.id.mail);
 
         callGraphButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -47,24 +33,7 @@ public class MainActivity extends AppCompatActivityRest {
             }
         });
 
-        signOutButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                actionLogout();
-            }
-        });
-
-        mailButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mailButtonClicked();
-            }
-        });
-
         actionLogin();
-    }
-
-    private void mailButtonClicked() {
-        startActivity(new Intent(this, MailActivity.class));
     }
 
     @Override
@@ -80,9 +49,7 @@ public class MainActivity extends AppCompatActivityRest {
 
         switch (outlookObjectCall) {
             case READUSER: {
-                /* Sets the Graph response */
-                TextView graphText = (TextView) findViewById(R.id.graphData);
-                graphText.setText(graphResponse.toString());
+                System.out.println(graphResponse);
             } break;
             case READMAIL: {
                 System.out.println(graphResponse);
@@ -99,12 +66,9 @@ public class MainActivity extends AppCompatActivityRest {
     /* Set the UI for successful token acquisition data */
     public void updateSuccessUI() {
         callGraphButton.setVisibility(View.INVISIBLE);
-        signOutButton.setVisibility(View.VISIBLE);
-        mailButton.setVisibility(View.VISIBLE);
         findViewById(R.id.welcome).setVisibility(View.VISIBLE);
         ((TextView) findViewById(R.id.welcome)).setText("Welcome, " +
                 Authentication.getAuthentication().getAuthResult().getUser().getName());
-        findViewById(R.id.graphData).setVisibility(View.VISIBLE);
     }
 
     /* Use MSAL to acquireToken for the end-user
@@ -142,10 +106,6 @@ public class MainActivity extends AppCompatActivityRest {
     /* Set the UI for signed-out user */
     private void updateSignedOutUI() {
         callGraphButton.setVisibility(View.VISIBLE);
-        signOutButton.setVisibility(View.INVISIBLE);
-        mailButton.setVisibility(View.INVISIBLE);
         findViewById(R.id.welcome).setVisibility(View.INVISIBLE);
-        findViewById(R.id.graphData).setVisibility(View.INVISIBLE);
-        ((TextView) findViewById(R.id.graphData)).setText("No Data");
     }
 }
