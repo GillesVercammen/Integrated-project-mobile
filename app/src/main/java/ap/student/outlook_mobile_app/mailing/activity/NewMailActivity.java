33d6c8b3e0806c080e4ee7547d119f6add9e210c
AlexanderRousseeuw <AlexanderRousseeuw@users.noreply.github.com>
2017-12-08 17:24:59 +0100

    Please fix this

--- app/src/main/java/ap/student/outlook_mobile_app/mailing/activity/NewMailActivity.java
+++ app/src/main/java/ap/student/outlook_mobile_app/mailing/activity/NewMailActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.EditText;
 
 import org.json.JSONObject;
 
+import ap.student.outlook_mobile_app.BLL.GraphAPI;
 import ap.student.outlook_mobile_app.DAL.OutlookObjectCall;
 import ap.student.outlook_mobile_app.Interfaces.AppCompatActivityRest;
 import ap.student.outlook_mobile_app.R;
@@ -17,21 +19,56 @@ import ap.student.outlook_mobile_app.R;
 
 public class NewMailActivity extends AppCompatActivityRest {
 
+    final EditText recipientTextField = (EditText) findViewById(R.id.recipientTextField);
+    final EditText subjectTextField = (EditText) findViewById(R.id.subjectTextField);
+    final EditText messageTextField = (EditText) findViewById(R.id.messageTextField);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_mail);
     }
 
-    //@Override
-    //public boolean onCreateOptionsMenu(Menu menu) {
-    //    // Inflate the menu; this adds items to the action bar if it is present.
-    //    getMenuInflater().inflate(R.menu.menu_send_mail, menu);
-    //    return true;
-    //}
-//
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_send_mail, menu);
+        return true;
+    }
+
     @Override
     public void processResponse(OutlookObjectCall outlookObjectCall, JSONObject response) {
 //
     }
+
+    public void sendMail(){
+        //get input fields, parse them to JSON, do call, show toaster for correct mesage
+
+        String recipient = recipientTextField.toString();
+        String subject = subjectTextField.toString();
+        String message = messageTextField.toString();
+
+        JSONObject JSON = new JSONObject();
+
+        //TextView subtitleTextView =
+        //        (TextView) rowView.findViewById(R.id.mail_list_subtitle);
+        //if (!jsonObject.getString("subject").isEmpty()) {
+        //    subtitleTextView.setText(jsonObject.getString("subject"));
+        //} else {
+        //    subtitleTextView.setText(R.string.no_subject);
+        //}
+        //if(jsonObject.getString("isRead").toLowerCase().equals("false")){
+        //    subtitleTextView.setTypeface(subtitleTextView.getTypeface(), Typeface.BOLD);
+        //}
+
+        //try {
+        //    new GraphAPI().postRequest(OutlookObjectCall.SENDMAIL, this, "/inbox/messages?$top=");
+//
+        //} catch (IllegalAccessException e) {
+        //    e.printStackTrace();
+        //}
+
+
+    }
+
 }
