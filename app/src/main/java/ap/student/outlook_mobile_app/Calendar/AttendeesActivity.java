866e4538e3a41652be528babbd693304f7e36753
AlekVT <alek.vtichelen@gmail.com>
2018-01-15 14:38:59 +0100

    altered contact to make it more workable

--- app/src/main/java/ap/student/outlook_mobile_app/Calendar/AttendeesActivity.java
+++ app/src/main/java/ap/student/outlook_mobile_app/Calendar/AttendeesActivity.java
@@ -1,11 +1,39 @@
 package ap.student.outlook_mobile_app.Calendar;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.gson.Gson;
+
+import ap.student.outlook_mobile_app.R;
 import ap.student.outlook_mobile_app.contacts.activity.ContactsActivity;
+import ap.student.outlook_mobile_app.contacts.model.Contact;
 
 /**
  * Created by alek on 12.01.18.
  */
 
 public class AttendeesActivity extends ContactsActivity {
+    private Gson gson;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        BottomNavigationView bottomNavigationItemView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationItemView.setVisibility(View.GONE);
+
+        gson = new Gson();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            setResult(201, new Intent().putExtra("contacts", "{}"));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
